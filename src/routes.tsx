45cf4b82a820261afa7b0ea08c6d90f3a55c0727
firatyedibela <yedibelafirat0@gmail.com>
2025-07-01 17:22:39 +0300

    feat: implement routes array

---
+++ src/routes.tsx
@@ -0,0 +1,27 @@
+type Route = {
+  title: string;
+  href: string;
+};
+
+export const routes: Route[] = [
+  {
+    title: 'home',
+    href: '#',
+  },
+  {
+    title: 'new',
+    href: '#',
+  },
+  {
+    title: 'popular',
+    href: '#',
+  },
+  {
+    title: 'trending',
+    href: '#',
+  },
+  {
+    title: 'categories',
+    href: '#',
+  },
+];
