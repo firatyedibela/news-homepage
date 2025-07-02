004b5cf8420ece76e4ba9a12f2bea1f27b41a493
firatyedibela <yedibelafirat0@gmail.com>
2025-07-02 16:28:42 +0300

    chore: import assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { MenuDesktop } from './components/MenuDesktop';
 import { MenuMobile } from './components/MenuMobile';
 import wIcon from './assets/images/w.svg';
+import retroPc from './assets/images/image-retro-pcs.jpg';
+import topLaptop from './assets/images/image-top-laptops.jpg';
+import gamingGrowth from './assets/images/image-gaming-growth.jpg';
 
 function App() {
   return (
-    <div id="container" className="min-h-screen px-4 pt-7 ">
+    <div id="container" className="min-h-screen px-4 pt-7">
       <header className="max-w-[1112px] m-auto">
         <nav className="flex justify-between items-center h-[28px]">
           <img className="w-[45px] h-[28px]" src={wIcon} alt="" />
