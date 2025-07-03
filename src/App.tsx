d53365e3caee184906014217767e2c1e5dba6de2
firatyedibela <yedibelafirat0@gmail.com>
2025-07-03 17:15:15 +0300

    feat: implement various articles section of main

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { MenuDesktop } from './components/MenuDesktop';
 import { MenuMobile } from './components/MenuMobile';
 import { Button } from './components/Button';
 import { ArticleCard } from './components/ArticleCard';
+import { Article } from './components/Article';
 import wIcon from './assets/images/w.svg';
 import retroPc from './assets/images/image-retro-pcs.jpg';
 import topLaptop from './assets/images/image-top-laptops.jpg';
@@ -11,7 +12,7 @@ import web3Desktop from './assets/images/image-web-3-desktop.jpg';
 
 function App() {
   return (
-    <div id="container" className="min-h-screen px-4 pt-7 font-display">
+    <div id="container" className="min-h-screen px-4 pt-7 pb-16 font-display">
       <header className="max-w-[1112px] m-auto mb-8">
         <nav className="flex justify-between items-center h-[28px]">
           <img className="w-[45px] h-[28px]" src={wIcon} alt="" />
@@ -32,6 +33,32 @@ function App() {
         </section>
         <section>
           <ArticleCard />
+        </section>
+        <section className="grid gap-8">
+          <Article
+            image={retroPc}
+            altText="Image of a retro pc"
+            prefix="01"
+            title="Reviving Retro PCs"
+            content="What happens when old PCs are given modern upgrades?"
+            href="#"
+          />
+          <Article
+            image={topLaptop}
+            altText="Image of a laptop keyboard"
+            prefix="02"
+            title="Top 10 Laptops of 2022"
+            content="Our best picks for various needs and budgets."
+            href="#"
+          />
+          <Article
+            image={retroPc}
+            altText="Image of a gamepad on top of a palm of a hand"
+            prefix="03"
+            title="The Growth of Gaming"
+            content="How the pandemic has sparked fresh opportunities."
+            href="#"
+          />
         </section>
       </main>
     </div>
