c397ffec0ae48464353e3a62aca15c29a039eecc
firatyedibela <yedibelafirat0@gmail.com>
2025-07-03 17:10:03 +0300

    feat: accept className as props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,14 @@ import type React from 'react';
 
 type ButtonProps = {
   children: React.ReactNode;
+  className: string;
 };
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ children, className }: ButtonProps) => {
   return (
     <button
       type="button"
-      className="px-8 py-3 bg-red-500 hover:bg-navy-950 text-navy-950 hover:text-white transition duration-150 cursor-pointer"
+      className={`px-8 py-3 bg-red-500 hover:bg-navy-950 text-navy-950 hover:text-white transition duration-150 cursor- ${className}`}
     >
       <span className="text-preset-7 uppercase">{children}</span>
     </button>
